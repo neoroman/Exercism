b62b987c2c9eded4c06601ebe6f0ba39fc383e53
EungShik <neoroman>
2024-01-26 09:01:40 +0900

    first import

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# The following comments should help you get started:
+# - Bash is flexible. You may use functions or write a "raw" script.
+#
+# - Complex code can be made easier to read by breaking it up
+#   into functions, however this is sometimes overkill in bash.
+#
+# - You can find links about good style and other resources
+#   for Bash in './README.md'. It came with this exercise.
+#
+#   Example:
+#   # other functions here
+#   # ...
+#   # ...
+#
+main () {
+    for x in "$@"; do
+        echo "One for $@, one for me"
+        exit
+    done
+    echo "One for you, one for me"
+}
+
+# call main with all of the positional arguments
+main "$@"
