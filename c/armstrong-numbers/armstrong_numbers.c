8a08df8c0668654316673c696b11353beb97839b
EungShik <neoroman>
2024-02-04 20:50:54 +0900

    Submit armstrong-number of C

---
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -0,0 +1,31 @@
+#include "armstrong_numbers.h"
+
+int getNumberOfDigits(int n);
+
+bool is_armstrong_number(int candidate) {
+
+    int numberOfDigits = getNumberOfDigits(candidate);
+    int sum = 0;
+    for (int i = candidate; i != 0; i /= 10)
+    {
+	    int num = i % 10;
+	    int n = 1;
+	    for (int j = 0; j < numberOfDigits; j++)
+		{
+			n *= num;
+		}
+	    sum += n;
+    }
+    return sum == candidate;
+}
+
+int getNumberOfDigits(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        n /= 10;
+        ++sum;
+    }
+    return sum;
+}
